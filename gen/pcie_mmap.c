403ba3c8f5bc983ac77427389bf870e12f7fc3bb
mtreddy <mtreddy@gmail.com>
2017-06-29 09:11:45 -0700

    Memapping pcie membar.

--- gen/pcie_mmap.c
+++ gen/pcie_mmap.c
@@ -3,13 +3,14 @@
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define MAP_SIZE (1024 * 1024)
-#define MAP_MASK (1024 * 1024)-1
+#define MAP_SIZE (1 * 1024 * 1024)
+#define MAP_MASK (1 * 1024 * 1024)-1
 int main(void)
 {
-    off_t          dev_base = 0x383ff8000000;
-    size_t         ldev = (1024 * 1024);
-    unsigned long  mask = (1024 * 1024)-1;
+    //off_t          dev_base = 0x383ff8000000;
+    unsigned long long           dev_base = 0x383ff8000000;
+    size_t         ldev = MAP_SIZE;
+    unsigned long  mask = MAP_MASK;
     int           *pu;
     void          *mapped_base;
     void          *mapped_dev_base;
@@ -18,12 +19,13 @@ int main(void)
     int            memfd;
 
     memfd = open("/dev/mem", O_RDWR | O_SYNC);
-    mapped_base = mmap(0, MAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, memfd, dev_base & ~MAP_MASK);
+    mapped_base = mmap64(0, MAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, memfd, dev_base);
+    printf("mapped_base = 0x%x\n", mapped_base);
     if (mapped_base == MAP_FAILED)
         errx(1, "mmap failure");
     mapped_dev_base = mapped_base + (dev_base & MAP_MASK);
     pu = mapped_dev_base;
-
+    printf("mapped_dev_base = 0x%llx\n", (unsigned long long *)mapped_dev_base);
     pcid = (int *) (((void *) pu) + 0xf0704);
 
     //printf("pu    = %08p\n", pu);
@@ -32,7 +34,10 @@ int main(void)
     //cid = *pcid;
     //printf("CID   = %x\n", cid);
 
-    munmap(mapped_base, ldev);
+    //munmap(mapped_base, ldev);
+    while(1) {
+        sleep(1);
+    }
     close(memfd);
 
     return (EXIT_SUCCESS);
