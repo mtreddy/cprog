ef77878d2829dfdd17ad87f73aa9f4abbb8012ae
Tirumala Marri <mtreddy@gmail.com>
2018-02-28 15:37:37 -0800

    removing some binries

--- klm/.hello-1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/mareddy/src/cprog/klm/hello-1.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/mareddy/src/cprog/klm/hello-1.ko /home/mareddy/src/cprog/klm/hello-1.o /home/mareddy/src/cprog/klm/hello-1.mod.o
