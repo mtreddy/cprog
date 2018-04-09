47abf167688e4fea6705bffac6a9f3c6a2495980
Tirumala Marri <mtreddy@gmail.com>
2018-04-08 22:03:21 -0700

    cpp file for top view

--- ds/trees/btree_top_view.cpp
+++ ds/trees/btree_top_view.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-typedef struct node_s{
-    struct node_s *left;
-    struct node_s *right;
+struct node{
+    struct node *left;
+    struct node *right;
     int data;
-}node;
+};
 void top_view(node *root, int level, int hlevel){
 	if(root == NULL)
         return;
     top_view(root->left, level+1, hlevel-1);
     if(abs(level) == abs(hlevel)){
-        cout << root->data << " ";
+        cout << root->data << " " << level << " " << hlevel << endl;
+        if(hlevel == 0) {
+            level = 0;
+        }
     }
     top_view(root->right, level+1, hlevel+1);
     return;
 }
 
 node *new_node(int data){
-    node *node = new node;
-    node->data = data;
-    node->left = node->right = NULL;
-    return node;
+    struct node* nd = new node;
+    nd->data = data;
+    nd->left = nd->right = NULL;
+    return nd;
 }
 int main(){
     node *root = new_node(1);
@@ -30,6 +33,8 @@ int main(){
     root->left->right = new_node(4);
     root->left->right->right = new_node(5);
     root->left->right->right->right = new_node(6);
+    cout << endl;
     top_view(root,0,0);
+    cout << endl;
     return 0;
 }
