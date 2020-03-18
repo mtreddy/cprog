1857d5663a4d4983686eaca651e0397e5414ea1b
Tirumala Marri <tirmarri@amd.com>
2020-03-18 17:54:54 -0500

    added dynmic prog version of  cut rod

--- algo/dyn_prog/cut_rod.c
+++ algo/dyn_prog/cut_rod.c
@@ -16,9 +16,39 @@ int cut_rod(int *price, int n)
         max_val = max(max_val, price[i] + cut_rod(price, n-i-1));
     return max_val;
 }
+
+int mem_cut_rod_aux(int *price, int n, int *r)
+{
+	int q = -1;
+	if (r[n] >= 0)
+		return r[n];
+
+	if (n < 0)
+		q = 0;
+	else {
+		int i;
+		q = -1;
+		for(i=0; i <= n; i++){
+			q = max(q, price[i]+mem_cut_rod_aux(price, n-i-1, r));
+		}
+	}
+	r[n] = q;
+	return q;
+}
+int mem_cut_rod(int *price, int n)
+{
+	int r[sizeof(len)/sizeof(int)];
+	int max_val = 0;
+    for(int i=0; i <= n; i++) 
+		r[i] = -1;
+	return mem_cut_rod_aux(price, n, r);
+
+}
 int main ()
 {
     int size = sizeof(len)/sizeof(len[0]);
-    printf(" Maximum attainable value is %d\n", cut_rod(len, size));
+//printf(" Maximum attainable value is %d\n", cut_rod(len, size));
+    printf(" Maximum attainable value is %d\n", mem_cut_rod(len, size-1));
+
     return 0;
 }
