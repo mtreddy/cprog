e7291590686559a2c0a34d0351b70fddb7c0fdf2
Tirumala <tirumala.marri@axiao.com>
2021-09-04 23:31:03 -0700

    Adding command line support

--- math/crypto/ec_point_multi.c
+++ math/crypto/ec_point_multi.c
@@ -1,3 +1,7 @@
+/*
+ * Example usage
+ *  ./ec_point_multi  "1" "05F939258DB7DD90E1934F8C70B0DFEC2EED25B8557EAC9C80E2E198F8CDBECD86B12053" "03676854FE24141CB98FE6D4B20D02B4516FF702350EDDB0826779C813F0DF45BE8112F4"
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <openssl/bio.h>
@@ -8,22 +12,7 @@ void init_ssl() {
 	SSL_load_error_strings();
 	SSL_library_init();
 }
-# if 0
-struct test_x_y {
-		const char *k, *x, *y;
-};
-static const struct test_x_y tp_x_y [] = {
-		{
-		/*k */
-		"1",
-	    /*x  */
-		"05F939258DB7DD90E1934F8C70B0DFEC2EED25B8557EAC9C80E2E198F8CDBECD86B12053",
-	   /*	y */
-		"03676854FE24141CB98FE6D4B20D02B4516FF702350EDDB0826779C813F0DF45BE8112F4",
-		},
-};
-#endif
-int main() {
+int main(int argc, char** argv) {
 	EC_KEY   *eckey = NULL; /*"NID_sect283r1"*/
 	const EC_GROUP *group = NULL;
 	EC_POINT *pout = NULL;
@@ -38,6 +27,24 @@ int main() {
 	pout = EC_POINT_new(group);
 	Q_CHECK = EC_POINT_new(group);
 	ctx = BN_CTX_new();
+	if(argc == 4) { 
+		struct test_x_y *tpp_x_y;
+		tpp_x_y->k = argv[1];
+		tpp_x_y->x = argv[2];
+		tpp_x_y->y = argv[3];
+		BN_hex2bn(&x, tpp_x_y[i].x); 
+		BN_hex2bn(&y, tpp_x_y[i].y); 
+		BN_dec2bn(&k, tpp_x_y[i].k); 
+		EC_POINT_mul(group, pout, k, NULL, NULL, ctx);
+		EC_POINT_set_affine_coordinates(group, Q_CHECK, x, y, ctx);
+		int val = EC_POINT_cmp(group, pout, Q_CHECK, ctx);
+		printf("k:%s\n", tp_x_y[i].k);
+		if (val == 0) 
+			printf("Matches\n");
+		else {
+			printf("Doesnt Matche %d\n", val);
+		}
+	}else{
 	for(i = 0; i < sizeof(tp_x_y)/sizeof(tp_x_y[0]); i++) {
 		BN_hex2bn(&x, tp_x_y[i].x); 
 		BN_hex2bn(&y, tp_x_y[i].y); 
@@ -64,6 +71,7 @@ int main() {
 		    	printf("\n");
 #endif
 		}
+	}
 	}
     BN_CTX_free(ctx);
 }
