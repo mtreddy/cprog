30686bd8407515d7f81af2ad1b9673891b611ff7
Tirumala Marri <mtreddy@gmail.com>
2025-02-24 18:59:27 -0800

    swapping code changes

--- gen/swap_num.c
+++ gen/swap_num.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
-void swap(int *v1, int *v2) {
+void swap_add(int *v1, int *v2) {
 	*v1 = *v1 + *v2;
 	*v2 = *v1 - *v2;
 	*v1 = *v1 - *v2;
+}
+void swap_xor(int *v1, int *v2) {
+	*v1 = *v1 ^ *v2;
+	*v2 = *v1 ^ *v2;
+	*v1 = *v1 ^ *v2;
 }
 
 int main(void) {
@@ -11,6 +16,8 @@ int main(void) {
 	int var2 = 200;
 
 	printf("var1=%d var2=%d\n", var1, var2);
-	swap(&var1, &var2);
+	swap_add(&var1, &var2);
+	printf("var1=%d var2=%d\n", var1, var2);
+	swap_xor(&var1, &var2);
 	printf("var1=%d var2=%d\n", var1, var2);
 }
